8cfc6c2388e66c5a8cd9921e192370e61f67f4ac
Aditya Saxena <Aditya_Saxena@mckinsey.com>
2015-08-20 15:32:13 +0530

    calling a microservice within a microservice

--- sales-tax1.js
+++ sales-tax1.js
@@ -1,31 +1,42 @@
 var seneca = require('seneca')();
+var _ = require('lodash');
 
-function resultObj (rate, net) {
-  return {
-   total:  net * (1 + rate  || 0)
- };
+seneca.add({
+    cmd: 'salestax'
+}, function(args, callback) {
+    var rate = args.rate === null ? 0 : args.rate;
+    var total = args.net * (1 + rate);
+    callback(null, {total: total});
+});
+
+function addCountry(country, callback) {
+    return function(err, result) {
+        callback(err, _.extend(result, {country: country}));
+    }
 }
 
 seneca.add({
-    cmd:'salestax'
+    cmd:'salestax',
+    country: 'US'
 }, function(args, callback){
-  var rate = 0.3;
-  callback(null, resultObj(rate, args.net));
+    var rate = 0.5;
+    seneca.act({
+        cmd: 'salestax',
+        rate: rate,
+        net: args.net
+    }, addCountry(args.country, callback));
 });
 
 seneca.add({
   cmd: 'salestax',
   country: 'India'
 }, function(args, callback) {
-  var rate = 0.4;
-  callback(null, resultObj(rate, args.net));
-});
-
-seneca.act({
-  cmd: 'salestax',
-  net: 100
-}, function(err, result) {
-   console.log( err, result);
+    var rate = 0.4;
+    seneca.act({
+        cmd: 'salestax',
+        rate: rate,
+        net: args.net
+    }, addCountry(args.country, callback));
 });
 
 seneca.act({
@@ -34,4 +45,12 @@ seneca.act({
   country: 'India'
 }, function(err, result) {
    console.log( err, result);
+});
+
+seneca.act({
+    cmd: 'salestax',
+    net: 100,
+    country: 'US'
+}, function(err, result) {
+    console.log( err, result);
 });
