7ddf89462d13e99cb513aea4bbd7a9c7bc843b49
Aditya Saxena <Aditya_Saxena@mckinsey.com>
2015-08-20 15:51:42 +0530

    add a state property

---
+++ sales-tax2.js
@@ -0,0 +1,85 @@
+var seneca = require('seneca')();
+var _ = require('lodash');
+
+seneca.add({
+    cmd: 'salestax'
+}, function(args, callback) {
+
+    var rate = args.rate === null ? 0 : args.rate;
+    var total = args.net * (1 + rate);
+
+    callback(null, {total: total});
+});
+
+function addCountry(country, callback) {
+    return function(err, result) {
+        callback(err, _.extend(result, {country: country}));
+    }
+}
+
+seneca.add({
+    cmd:'salestax',
+    country: 'US'
+}, function(args, callback){
+    var rate = 0.5;
+    seneca.act({
+        cmd: 'salestax',
+        rate: rate,
+        net: args.net
+    }, addCountry(args.country, callback));
+});
+
+seneca.add({
+    cmd:'salestax',
+    country: 'US',
+    state: 'Arizona'
+}, function(args, callback){
+    var rate = 0.2;
+    seneca.act({
+        cmd: 'salestax',
+        rate: rate,
+        net: args.net
+    }, addCountry(args.country, callback));
+});
+
+seneca.add({
+  cmd: 'salestax',
+  country: 'India'
+}, function(args, callback) {
+    var rate = 0.4;
+    seneca.act({
+        cmd: 'salestax',
+        rate: rate,
+        net: args.net
+    }, addCountry(args.country, callback));
+});
+
+
+//acting
+seneca.act({
+  cmd: 'salestax',
+  net: 100,
+  country: 'India'
+}, function(err, result) {
+   console.log( err, result);
+});
+
+seneca.act({
+    cmd: 'salestax',
+    net: 100,
+    country: 'US'
+}, function(err, result) {
+    console.log( err, result);
+});
+
+seneca.act({
+    cmd: 'salestax',
+    net: 100,
+    country: 'US',
+    state: 'Arizona'
+}, function(err, result) {
+    console.log( err, result);
+});
+
+//SENECA FLAG TO DEBUG WITH
+// -- seneca.log.all
