ba6746209b046226fef62128b510db0d19901e17
Aditya Saxena <Aditya_Saxena@mckinsey.com>
2015-08-21 13:03:25 +0530

    hapi test

---
+++ hapi-test.js
@@ -0,0 +1,31 @@
+var Hapi = require('hapi');
+var Inert = require('inert');
+
+// Create a server with a host and port
+var server = new Hapi.Server();
+server.connection({ 
+    host: 'localhost', 
+    port: Number(process.argv[2] || 8000)
+});
+
+server.register(Inert, function (err) {
+    if (err) throw err;
+});
+
+// Add the route
+server.route({
+    method: 'GET',
+    path:'/foo/bar/baz/{filename}', /// the first few parameters do not indicate the directory, it is just indicatory of your route
+    handler: {
+        directory: {
+            path: './public'
+        }
+    }
+});
+
+// Start the server
+server.start(function() {
+     console.log('Server running at:', server.info.uri);
+});
+
+// had to put in a empty line at the end of index.html to make the program pass
